7af552f3a002da762d4f96478622c1c373305559
jose.fernandez159 <fernandez1c3d@gmail.com>
2022-05-08 11:18:41 -0300

    second version widget

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,9 @@
 import { Widget } from "../../Components/Widgets/Widgets";
-import { Test } from "../../Components/Room/Room";
+
 
 document.body.style.backgroundColor = '#FFCBE0';
 export function Home(){
  return (
-   <main>
-<Test/>
 <Widget/>
-   </main>
  )
 }
